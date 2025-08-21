1da79b53555d4871464ba0d4514181d89d719365
will45-dev <will.haggard@ucas-edu.net>
2025-08-21 10:11:19 -0600

    seson starter

---
+++ Starters/season.py
@@ -0,0 +1,10 @@
+# WH 2nd seasons
+
+last_name = input("what is your last name:")
+season = input("what is you favorite season:")
+if (season == "fall"): {
+print ("Wow", last_name,"I like",season,"too")
+}
+else:{
+print ("Wow", last_name,"I hate that",season)
+}
