5f4f0bef985725e415ddc63e588fdbf4edacb1a7
will45-dev <will.haggard@ucas-edu.net>
2025-08-25 10:50:33 -0600

    project one

---
+++ my_Projects_for_fun/project_one/project_one.py
@@ -0,0 +1,7 @@
+# WH 2nd not for class
+
+nubers_txt = "C:/Users/william.haggard/Desktop/2nd haggard, william/WH_CP1/my_Projects_for_fun/project_one/numbers.txt"
+
+with open(nubers_txt, "a") as number:
+  number.write("he")
+
