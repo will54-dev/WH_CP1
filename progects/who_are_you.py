cb94230dd0a339d780ee5693f574056e6cf11364
will45-dev <will.haggard@ucas-edu.net>
2025-08-25 10:04:24 -0600

    who are you progect

--- progects/who_are_you.py
+++ progects/who_are_you.py
@@ -1,9 +1,7 @@
 # WH 2nd who are you progect
 
 user_name = input("give you name: ")
-#user_age = input("give you age")
-#user_color = input("give your favorite color")
-with open("users.txt") as e:
-e.write(user_name)
-with open("users.txt", "w") as e:
-    print(e.read())
+user_age = input("give you age: ")
+user_color = input("give your favorite color: ")
+
+print("hello", user_name,"your favorite is",user_color,"and your age is",user_age)
