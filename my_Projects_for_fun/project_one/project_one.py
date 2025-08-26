40befc1395546b3b253e65c02a8425a74ab96f42
will45-dev <will.haggard@ucas-edu.net>
2025-08-26 10:49:51 -0600

    project one

--- my_Projects_for_fun/project_one/project_one.py
+++ my_Projects_for_fun/project_one/project_one.py
@@ -1,7 +1,13 @@
 # WH 2nd not for class
 
-nubers_txt = "C:/Users/william.haggard/Desktop/2nd haggard, william/WH_CP1/my_Projects_for_fun/project_one/numbers.txt"
+names_txt = "C:/Users/william.haggard/Desktop/2nd haggard, william/WH_CP1/my_Projects_for_fun/project_one/numbers.txt"
+name =  input("give your name: ")
 
-with open(nubers_txt, "a") as number:
-  number.write("he")
+with open(names_txt, "r") as name_list:
+  the_name = name_list.read()
+  for a_name in the_name:
+    if a_name in name:
+      print("yes")
 
+with open(names_txt, "w") as name_write:
+  name_write.write(name)
