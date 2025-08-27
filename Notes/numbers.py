4864bcd93bed2b89c21a34bd4b3e1acc36c6344f
will45-dev <will.haggard@ucas-edu.net>
2025-08-27 10:50:48 -0600

    Intergers and Floats Notes

---
+++ Notes/numbers.py
@@ -0,0 +1,18 @@
+# WH 2nd Intergers and Floats Notes
+
+#Integer
+age = 32436587365487324644644637328839849273746578273477545892983774664637282
+temp = 568
+attantion = -20
+spoons = 4
+# a integer is a whole number
+
+#Float (floating point number)
+pi = 3.1415926
+pounds = 5.3
+emotional_damage = 100.01
+patience_level = -0.50
+# a float is a number that has a decimal
+
+#math
+print(8/4+7+-(8+2)%4**2)
