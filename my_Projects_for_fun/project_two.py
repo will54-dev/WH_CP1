55d00c1bcb5f4d8756b51d061161f2bd632463f9
will45-dev <will.haggard@ucas-edu.net>
2025-08-29 10:51:13 -0600

    notes

--- my_Projects_for_fun/project_two.py
+++ my_Projects_for_fun/project_two.py
@@ -1,7 +1,7 @@
 # WH 2nd not for class
 
-file_name = "C:/Users/william.haggard/Desktop/New Text Document.txt"
+file_name = "C:/Users/william.haggard/Desktop/Document.txt"
 
-with open(file_name, 'r') as file:
-    content = file.read()
+with open(file_name, 'w') as file:
+    content = file.write("e")
     print(content)
