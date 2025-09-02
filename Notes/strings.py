2a93a8eca5a5a0083722c2829822cfd041e1cd2e
will45-dev <will.haggard@ucas-edu.net>
2025-09-02 10:51:17 -0600

    notes

--- Notes/strings.py
+++ Notes/strings.py
@@ -1,1 +1,26 @@
 # WH 2nd strings notes
+
+# what is a string
+string = "content"
+#examples
+sentence = "the quick brown fox jumps over the laze dog!"
+name = 'william'
+month = "september",""''""''""''""
+
+# If I need to use ' or "
+name = "wil'l"
+best = '"my name is jeffffffffffffffffff"'
+
+funnny = "the dog jump into the lake"
+
+# length of string
+print(len(sentence))
+
+# escape character
+print('\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\twill said \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"don\'t die"')
+
+#concatinate
+print(name + " "+ funnny)
+
+#slicing
+print(name[2:3])
