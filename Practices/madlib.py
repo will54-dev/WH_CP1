255a5e0f63f37cb2bf704afcc0e9b9f1bfd0fd17
will45-dev <will.haggard@ucas-edu.net>
2025-09-03 10:50:52 -0600

    madlib

---
+++ Practices/madlib.py
@@ -0,0 +1,12 @@
+# WH 2nd madlib
+
+adjective = input("give an adjective: ")
+adjective_1 = input("give an adjective: ")
+meat = input("give a type of meat: ")
+room = input("give a room: ")
+verb = input("give a verb past tense: ")
+name = input("give a name: ")
+noun = input("give a noun: ")
+liquid = input("give a liquid: ")
+
+print("It was a " + adjective + " day. My mother was " + adjective_1 + " the food. I was having some " + meat + " in the " + room + ". Than " + name + " came in and had some " + meat + " too. I did " + verb + " until I tripped on a " + noun + ". Than drank " + liquid + ". The end.")
