90fcafb329ac6e8b21a7302327d5df4d292a90dc
will45-dev <will.haggard@ucas-edu.net>
2025-09-09 10:51:32 -0600

    calculartor

--- Practices/calculator.py
+++ Practices/calculator.py
@@ -1,6 +1,17 @@
 # WH 2nd calculator practice
 
+while_ch = 0
+
 
 def number_che(number):
-    for space_name in number:
-        if space_name in "12345678900.":
+    number_len = 0
+    for number_digit in number:
+        if number_digit in "1234567890.":
+            number_len +=1
+    if number_len == len(number):
+        return True
+        
+while while_ch == 0:
+    number_1 = input("give a number: ")
+    if number_che(number_1):
+        while_ch = 1
