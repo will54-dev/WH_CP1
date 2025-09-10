02b5b2068307a7118b22b2b355d556585cd2f1e0
will45-dev <will.haggard@ucas-edu.net>
2025-09-10 10:51:22 -0600

    shares projects

---
+++ projects/crew_Shares.py
@@ -0,0 +1,8 @@
+# WH 2nd crew shares projects
+
+money = float(input("how much money did they earn.\n"))
+crew_number = input("how many crew members are there.\n")
+captain_shares = 7
+first_mate_shares = 3
+crew_shares = 1
+
