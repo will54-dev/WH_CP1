b99b3dc23aa92b8dab806f7ca0fbd8ed16b18116
will45-dev <will.haggard@ucas-edu.net>
2025-09-16 10:50:36 -0600

    project_two for fun

--- my_Projects_for_fun/project_two.py
+++ my_Projects_for_fun/project_two.py
@@ -1,7 +1,10 @@
 # WH 2nd not for class
 
-file_name = "C:/Users/william.haggard/Desktop/Document.txt"
-
-with open(file_name, 'w') as file:
-    content = file.write("e")
-    print(content)
+file_name = "C:/Users/william.haggard/Desktop/"
+while_number = 1
+while_print = int(input("how many: "))+1
+if while_print < 501:
+    while while_number != while_print :
+        with open(file_name+f"Document{while_number}.txt", 'w') as file:
+            content = file.write("hi")
+        while_number += 1
