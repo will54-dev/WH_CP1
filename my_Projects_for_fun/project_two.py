3d4ca2f5e2c7a6bcfd4354e4d362c0ae407659cc
will45-dev <will.haggard@ucas-edu.net>
2025-09-17 10:50:41 -0600

    project two for fun

--- my_Projects_for_fun/project_two.py
+++ my_Projects_for_fun/project_two.py
@@ -1,10 +1,11 @@
 # WH 2nd not for class
 
-file_name = "C:/Users/tyler.hurst/Desktop/"
+file_name = "C:/Users/william.haggard/Desktop/"
 while_number = 1
 while_print = int(input("how many: "))+1
+doc_tet = input("give the text")
 if while_print < 501:
     while while_number != while_print :
         with open(file_name+f"Document{while_number}.txt", 'w') as file:
-            content = file.write("hi")
+            content = file.write(doc_tet)
         while_number += 1
