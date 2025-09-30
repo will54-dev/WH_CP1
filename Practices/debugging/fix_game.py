861d8d9ba0a406b3423c2a485afba1e9af896f2f
will54-dev <will.haggard@ucas-edu.net>
2025-09-30 18:44:13 +0000

    fix game

--- Practices/debugging/fix_game.py
+++ Practices/debugging/fix_game.py
@@ -3,28 +3,19 @@ import random
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 number_to_guess = random.randint(1, 100)
-number_to_guess = 2
 max_attempts = 10
-attempts = 1
-game_run = True
-guess_run = True
-while game_run:
-    while guess_run:
-        try:
-            guess = int(input("Enter your guess: "))
-            guess_run = False
-        except:  
-            guess_run = True
-    if guess == number_to_guess:
-        print("Congratulations! You've guessed the number!")
-        game_run = False
-    elif attempts >= max_attempts:
+attempts = 0
+game_over = False
+while not game_over:
+    guess = int(input("Enter your guess: "))
+    if attempts >= max_attempts:
         print(f"Sorry, you've used all {max_attempts} attempts. The number was {number_to_guess}.")
-        game_run = False
+        game_over = True
+    elif guess == number_to_guess:
+        print("Congratulations! You've guessed the number!")
+        game_over = True
     elif guess > number_to_guess:
         print("Too high! Try again.")
     elif guess < number_to_guess:
-        print("Too low! Try again.")
+        print("Too low! Try again.") 
     attempts += 1
-    continue
-print("Game Over. Thanks for playing!")
