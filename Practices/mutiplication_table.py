0b563645d79b14a6240f7d11eabb101e5625a0d5
will54-dev <will.haggard@ucas-edu.net>
2025-10-01 18:46:14 +0000

    mutiplication tables

--- Practices/mutiplication_table.py
+++ Practices/mutiplication_table.py
@@ -1,12 +1,14 @@
 # WH 2nd multiplication table practice
 
 numbers = range(1,13)
-lists = []
-listf = []
+lists = "1"
+print(numbers)
 for x in numbers:
     for c in numbers:
-        lists.append(x*c)
-        print(lists)
-    listf.extend(lists)
-    lists = []
-print(listf)
+        if x*c < 10:
+            lists = f"{lists} {(x*c)}"
+        elif x*c < 100:
+            lists = f"{lists} {(x*c)}"
+
+    print(lists)
+    lists = str(x+1)
