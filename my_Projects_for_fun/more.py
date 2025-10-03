f5775494583a21816229f7a0fdf32df25873f321
will45-dev <will.haggard@ucas-edu.net>
2025-10-03 10:50:44 -0600

    more

--- my_Projects_for_fun/more.py
+++ my_Projects_for_fun/more.py
@@ -5,7 +5,7 @@ file_locate = f"C:/Users/william.haggard/Desktop/more_{number}.py"
 copy_place =  "P:/Haggard, William/WH_CP1/my_Projects_for_fun/more.py"
 with open(copy_place, 'r') as text:
     text = text.read()
-    text = text.replace(str(number),str(number+1))
+    text = text.replace(f"number = {0}",f"number = {number+1}")
     print(text)
 with open(file_locate, 'w') as x:
     x = x.write(text)
