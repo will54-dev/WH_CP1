346b9550f098a70a6c31d6733aac5f5eb6d9a2ad
will4-dev <will.haggard@ucas-edu.net>
2025-10-20 10:51:27 -0600

    functions notes

---
+++ Notes/functions_notes.py
@@ -0,0 +1,14 @@
+# WH 2nd function notes
+num = 0
+def add(x, y):
+    return x+y
+print(add(45,79))
+print(add(100,63))
+print(add(84,70))
+
+while num < add(5,5):
+    print("Duck")
+    num += 1
+print("goose")
+
+print(add(213,add(672954578213,75884545)))
