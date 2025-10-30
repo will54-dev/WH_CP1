5eedad4a58bfdbc836b5fca1fdcd0ed23c102046
will4-dev <will.haggard@ucas-edu.net>
2025-10-30 10:52:28 -0600

    turtle race

--- Practices/turtle_race.py
+++ Practices/turtle_race.py
@@ -6,7 +6,7 @@ import random
 
 #functions
 #turtle set up function needs position, color, and speed
-def turtle_set(color,position):
+def turtle_set(color,x_position,y_posirion):
     #sets the turtle
     name = turtle.Turtle()
     #sets the color
@@ -14,41 +14,46 @@ def turtle_set(color,position):
     #sets the shape
     name.shape("turtle")
     #sets the speed
-    name.speed(100)
+    name.speed(1)
     #set the position
-    name.teleport(0,position)
+    name.teleport(y_posirion,x_position)
     #exports the turtle
     return name
 
+# a function that doos the movment of the turtle
+def turtle_move(name,rand):
+    #set the amount it moves
+    move = random.randint(20,rand)
+    #moves int that amount
+    name.forward(move)
+
+
 #code
-#set up the turtles with difrent speeds and hide some
-t0 = turtle_set("black",1000,250)
+#set up the turtles and hide some
+t0 = turtle_set("black",250,300)
 t0.hideturtle()
-t1 = turtle_set("red",random.randint(100,200),200)
-t2 = turtle_set("green",random.randint(70,210),150)
-t3 = turtle_set("blue",random.randint(50,170),100)
-t4 = turtle_set("yellow",random.randint(60,200),50)
-t5 = turtle_set("purple",random.randint(80,190),0)
-#make a turtle 0 move to the end
-t0.teleport(300,t0.ycor())
-#make him draw the finsih line
+t1 = turtle_set("red",200,-300)
+t2 = turtle_set("green",150,-300)
+t3 = turtle_set("blue",100,-300)
+t4 = turtle_set("yellow",50,-300)
+t5 = turtle_set("purple",0,-300)
+#make t0 draw the finsih line
 t0.right(90)
 t0.forward(300)
 #make the turles run
-t1.forward(300)
-t2.forward(300)
-t3.forward(300)
-t4.forward(300)
-t5.forward(300)
-#if the turtle 1 x position = turtle 0 x position
-    #display turtle 1 a the winer
-#if the turtle 2 x position = turtle 0 x position
-    #display turtle 1 a the winer
-#if the turtle 3 x position = turtle 0 x position
-    #display turtle 1 a the winer
-#if the turtle 4 x position = turtle 0 x position
-    #display turtle 1 a the winer
-#if the turtle 5 x position = turtle 0 x position
-    #display turtle 1 a the winer
-
-turtle.done()
+#loop
+loop = True
+while loop:
+    #moves all the turtles
+    #loop for t1, t2, t3, 4, t5 as a var
+    for var in [t1,t2,t3,t4,t5]:
+        #uses turtle move wite the var as name and a random and rand
+        turtle_move(var, 40)
+        #check if the turtle won
+        if var.xcor() >= t0.xcor():
+            var.forward(400)
+            #show winer
+            print(f"{var.color()[1]} is the winner")
+            #ends the loops
+            loop = False
+            break
