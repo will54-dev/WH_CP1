d728f94ea7a1342e03724a8796e99c16adc88213
will4-dev <will.haggard@ucas-edu.net>
2025-11-06 10:51:08 -0700

    maze

--- Practices/maze_genorator.py
+++ Practices/maze_genorator.py
@@ -1,79 +1,132 @@
 # Wh 2nd maze genorator 
 
 #impors 
-#imppot tutle of the drawing 
+#impot turtle of the drawing
+import turtle 
 #import random to randomly genorate the maze
+import random
 
 #the variables
-#the pixel scaile the maze size 
-#the x lenght 
+#the pixel scaile the maze size
+size = 100
+#the x length
+x_len = 4
 #the y length
+y_len = 6
 #the side walls x
+wall_x = []
 #the vetical walls y
+wall_y = []
 
 #the functions
 
-#a fuction that takes: the the y or x lenght, the x or y length, and hole
+#a fuction that takes: the the y or x length, the x or y length, and hole
+def wallRandom(length_1,length_2,hole):
+    #set output
+    output = []
     #if hole is true
-        #set a nother disposibal variable as a 1 list as 0
+    if hole:
+        #set another disposibal variable as a 1 list as 0
+        list = [0]
         #set x or y length to sumthing disposible - 1
+        length = length_2 - 1
         #a loop that repeats x or y length disposible
-                #add a 1 to the 1 list 
-        #makeing the loop stop
+        while length != 0:
+            #add a 1 to the 1 list 
+            list.append(1)
+            #makeing the loop stop
+            length -= 1
         #set an output veriable to 1 list 
-    #else 
-        #set a nother disposibal variable as a 1 list as empty
+        output.append(list)
+    #else
+    else:
+        #set another disposibal variable as a 1 list as empty
+        list = []
         #set x or y length to sumthing disposible
+        length = length_2
         #a loop that repeats x or y length disposible
-                #add a 1 to the 1 list 
-        #makeing the loop stop
-        #set an output veriable to 1 list 
-        #set an output veriable to a line with out a hole in it
+        while length:
+            #add a 1 to the 1 list 
+            list.append(1)
+            #makeing the loop stop
+            length -= 1
+        #set an output veriable to 1 list
+        output.append(list)
 
     #set y or x length to sumthing disposible
+    length_1d = length_1
     #a loop that repeats y or x length disposible
+    while length_1d != 0:
         #set x or y length to sumthing disposible
+        length_2d = length_2
+        #set list 1 as empty
+        list = []
         #a loop that repeats x or y length disposible
-                #set a nother disposibal variable as a 1 list as empty
-                #add a random number of 0-1 to the 1 list 
-        #makeing the loop stop
+        while length_2d != 0:
+            #add a random number of 0-1 to the list 2
+            list.append(random.randint(0,1))
+            #makeing the loop stop
+            length_2d -= 1
         #add 1 list to output
-    #makeing the loop stop
+        output.append(list)
+        #makeing the loop stop
+        length_1d -= 1
 
+    #set disposibal variable as a 1 list as empty
+    list = []
     #if hole is true
-        #set a nother disposibal variable as a 1 list as empty
+    if hole:
         #set x or y length to sumthing disposible - 1
+        length = length_2 - 1
         #a loop that repeats x or y length disposible
-                #add a 1 to the 1 list 
-        #makeing the loop stop
+        while length != 0:
+            #add a 1 to the 1 list
+            list.append(1)
+            #makeing the loop stop
+            length -= 1
         #add 0 to 1 list
-        #set an output veriable to 1 list 
-    #else 
-        #set a nother disposibal variable as a 1 list as empty
+        list.append(0)
+        #add to output veriable 1 list
+        output.append(list)
+    #else
+    else:
         #set x or y length to sumthing disposible
+        length = length_2
         #a loop that repeats x or y length disposible
-                #add a 1 to the 1 list 
-        #makeing the loop stop
+        while length != 0:
+            #add a 1 to the 1 list
+            list.append(1)
+            #makeing the loop stop
+            length -= 1
         #set an output veriable to 1 list 
-        #set an output veriable to a line with out a hole in it
+        output.append(list)
 
     #outputs output
+    return output
 
 #a function tha checkers if it is solvable takes: side walls x, #the vetical walls y, start, and stop
-    #set checked positions 
+def checkMaze(side_x,vetical_y,start,stop):
+    #set checked positions
+    checked = []
     #set check item to start
+    item = [start]
 
     #loop
-        #if y item [check item [0 of 0]] of [check item [0 of 1]] is 0 and, check item [0 of 0] - 1 > or = 0
+    while True:
+        #if y item [check item [0 of 0]] of [check item [0 of 1]] is 0 and, check item [0 of 0] - 1 >= 0
+        if vetical_y[item[0][0]][item[0][1]] == 0 and item[0][0] -1 >= 0:
+            #add check item [0 of 0] and check item [0 of 1] - 1
+            item.append([item[0][0],item[0][1]-1])
+
+        #if x item [check item [0 of 1] + 1] of [check item [0 of 0]] is 0 and, check item [0 of 0] - 1 <= length side walls x
+        if vetical_y[item[0][1] + 1][item[0][0]] == 0 and item[0][1] -1 <= 0:
+            #add check item [0 of 0]  + 1 and check item [0 of 1]
+            item.append([item[0][0] + 1 ,item[0][1]])
+
+        #if y item [check item [0 of 0] + 1] of [check item [0 of 1]] is 0 and, check item [0 of 0] - 1 <= length side walls y
             #add check item [0 of 0] and check item [0 of 1] - 1
 
-        #if x item [check item [0 of 1] + 1] of [check item [0 of 0]] is 0 and, check item [0 of 0] - 1 < or = lenght side walls x
-            #add check item [0 of 0] and check item [0 of 1] - 1
-
-        #if y item [check item [0 of 0] + 1] of [check item [0 of 1]] is 0 and, check item [0 of 0] - 1 < or = lenght side walls y
-            #add check item [0 of 0] and check item [0 of 1] - 1
-
-        #if x item [check item [0 of 1]] of [check item [0 of 0]] is 0 and, check item [0 of 0] - 1 > or = 0
+        #if x item [check item [0 of 1]] of [check item [0 of 0]] is 0 and, check item [0 of 0] - 1 >= 0
             #add check item [0 of 0] and check item [0 of 1] - 1
 
         #check item remove item 0
@@ -83,11 +136,35 @@
             #output false
 
 # a function that draws the maze needs: distance , side walls x, and the vetical walls y
-#set count y to 0
-# #set count x to 0
+
+    #start sets this to be able to senter the maze
+
     #loop for walls x in side walls x
+        #set count to 0
         #loop for wall in side  walls x
-            #if side walls x itme count x of count y
-            #move the turtle distance 
-            #add 1 to count y
-        #add 1 to count x
+            #if wall is 1
+                #tutle pen down
+                #move the turtle distance
+            #if wall is 0
+                #tutle pen up
+                #move the turtle distance
+            #add 1 to count
+        #teloport the turtle up distance and left count * distance
+
+    #teloport to starting postioion
+
+    #rotate the tutle left 90
+    #loop for walls y in side walls y
+        #set count to 0
+        #loop for wall in side  walls y
+            #if wall is 1
+                #tutle pen down
+                #move the turtle distance
+            #if wall is 0
+                #tutle pen up
+                #move the turtle distance
+            #add 1 to count
+        #theloport th turtle up distance and left count * distance
+
+print(wallRandom(y_len,x_len,True))
+print(wallRandom(x_len,y_len,False))
