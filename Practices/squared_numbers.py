1d2817358e65a4eebaabecdaeac1aa0f145aa4c9
will54-dev <will.haggard@ucas-edu.net>
2025-11-21 05:30:18 +0000

    squared numbers

--- Practices/squared_numbers.py
+++ Practices/squared_numbers.py
@@ -1,3 +1,2 @@
 #Wh 2nd square numbers, list to run the program
-'[print(f"{i**.5}^2={i}")for(i)in map(lambda n:n**2,range(99))]'
-[print(f"{i**.5}^2={i}")for(i)in map(lambda n:n**2,[x*0+list(map(lambda i:float(i)if i.isnumeric()else 0,input(f"number {x+1}:")))[0]for x in range(20)])]
+[print(f"{i**.5}^2={i}")for(i)in map(lambda n:n**2,range(99))]
